49aa6d42fd39604bfaf8b1d77bd5474e346fd6e5
Huba Hanna <gubanna.11@gmail.com>
2024-08-10 13:45:10 +0300

    Catalog draft/errors fixes (#1)
    * Docker and docker-compose files; some errors
    
    * FIxed errors: mediatr configuration & path in db seed

--- Services/Catalog/Catalog.Infrastructure/Data/TypeContextSeed.cs
+++ Services/Catalog/Catalog.Infrastructure/Data/TypeContextSeed.cs
@@ -9,10 +9,9 @@ public static class TypeContextSeed
     public static void SeedData(IMongoCollection<ProductType> typeCollection)
     {
         bool checkTypes = typeCollection.Find(b => true).Any();
-        string path = Path.Combine("Data", "Seed", "types.json");
         if (!checkTypes)
         {
-            var typesData = File.ReadAllText(path);
+            var typesData = File.ReadAllText("../Catalog.Infrastructure/Data/SeedData/types.json");
             var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
             if (types != null)
             {
