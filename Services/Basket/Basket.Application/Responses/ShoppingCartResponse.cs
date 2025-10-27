6b1bec377ab1e21d7797533df5f76e16504bd5ab
Hanna Huba <gubanna.11@gmail.com>
2025-10-27 23:16:38 +0200

    Add packages, repository, mapper, reponses

---
+++ Services/Basket/Basket.Application/Responses/ShoppingCartResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace Basket.Application.Responses;
+
+public class ShoppingCartResponse
+{
+    public string UserName { get; set; }
+
+    public List<ShoppingCartItemResponse> Items { get; set; }
+
+    public decimal TotalPrice
+    {
+        get
+        {
+            return Items.Sum(i => i.Price * i.Quantity);
+        }
+    }
+
+    public ShoppingCartResponse()
+    {
+    }
+
+    public ShoppingCartResponse(string userName)
+    {
+        UserName = userName;
+    }
+}
